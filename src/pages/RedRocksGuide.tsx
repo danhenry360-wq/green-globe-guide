2e9fd74c7743e6cfbce07f4b07038f9e28475358
danhenry360-wq <danhenry360@gmail.com>
2025-12-13 15:51:32 +0100

    Update RedRocksGuide.tsx

--- src/pages/RedRocksGuide.tsx
+++ src/pages/RedRocksGuide.tsx
@@ -19,7 +19,7 @@ import {
   Bus, Bike, MapPinned, Snowflake, Sun, Leaf, Flower2,
   Music, Palette, Beer, TreePine, Mountain, Camera,
   AlertTriangle, Ban, Mail, Download, 
-  Compass, Bone
+  Compass, Bone, Waves
 } from "lucide-react";
 
 interface Dispensary {
